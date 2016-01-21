e08f839d7d241e3fd5ecb673056c0fda2ba80551
Pieter Frenssen <pieter@frenssen.be>
2016-01-21 17:25:06 +0200

    Use shorthand array syntax.

--- tests/src/Web/OgComplexWidgetTest.php
+++ tests/src/Web/OgComplexWidgetTest.php
@@ -41,23 +41,23 @@ class OgComplexWidgetTest extends WebTestBase {
    * Tests "field modes" of the OG reference widget.
    */
   function testFieldModes() {
-    $user1 = $this->drupalCreateUser(array('administer group', 'access content', 'create post content'));
-    $user2 = $this->drupalCreateUser(array('access content', 'create post content'));
+    $user1 = $this->drupalCreateUser(['administer group', 'access content', 'create post content']);
+    $user2 = $this->drupalCreateUser(['access content', 'create post content']);
 
     // Create group nodes.
-    $settings = array(
+    $settings = [
       'type' => 'group',
       OG_GROUP_FIELD . '[und][0][value]' => 1,
-    );
+    ];
     $settings['uid'] = $user1->uid;
     $group1 = $this->drupalCreateNode($settings);
 
     $settings['uid'] = $user2->uid;
     $group2 = $this->drupalCreateNode($settings);
 
-    $settings = array(
+    $settings = [
       'type' => 'post',
-    );
+    ];
     $settings['uid'] = $user1->uid;
     $post1 = $this->drupalCreateNode($settings);
     og_group('node', $group1->nid, array('entity_type' => 'node', 'entity' => $post1));
@@ -86,30 +86,30 @@ class OgComplexWidgetTest extends WebTestBase {
    * Test non-accessible group IDs are saved, upon form submit.
    */
   function testHiddenGroupIds() {
-    $user1 = $this->drupalCreateUser(array('administer group', 'access content', 'create post content'));
-    $user2 = $this->drupalCreateUser(array('access content', 'create post content'));
+    $user1 = $this->drupalCreateUser(['administer group', 'access content', 'create post content']);
+    $user2 = $this->drupalCreateUser(['access content', 'create post content']);
 
     // Create group nodes.
-    $settings = array(
+    $settings = [
       'type' => 'group',
       OG_GROUP_FIELD . '[und][0][value]' => 1,
-    );
+    ];
     $settings['uid'] = $user1->uid;
     $group1 = $this->drupalCreateNode($settings);
 
     $settings['uid'] = $user2->uid;
     $group2 = $this->drupalCreateNode($settings);
 
-    $settings = array(
+    $settings = [
       'type' => 'post',
-    );
+    ];
     $settings['uid'] = $user1->uid;
     $post1 = $this->drupalCreateNode($settings);
-    og_group('node', $group1->nid, array('entity_type' => 'node', 'entity' => $post1));
-    og_group('node', $group2->nid, array('entity_type' => 'node', 'entity' => $post1));
+    og_group('node', $group1->nid, ['entity_type' => 'node', 'entity' => $post1]);
+    og_group('node', $group2->nid, ['entity_type' => 'node', 'entity' => $post1]);
 
     $this->drupalLogin($user2);
-    $this->drupalPost("node/$post1->nid/edit", array(), 'Save');
+    $this->drupalPost("node/$post1->nid/edit", [], 'Save');
 
     // Assert post still belongs to both groups, although user was able
     // to select only one.
@@ -125,19 +125,19 @@ class OgComplexWidgetTest extends WebTestBase {
     $user1 = $this->drupalCreateUser();
     $user2 = $this->drupalCreateUser();
 
-    $settings = array(
+    $settings = [
       'type' => 'group',
       OG_GROUP_FIELD . '[und][0][value]' => 1,
-    );
+    ];
     $settings['uid'] = $user1->uid;
     $group1 = $this->drupalCreateNode($settings);
 
-    og_group('node', $group1->nid, array('entity' => $user2, 'state' => OG_STATE_PENDING));
+    og_group('node', $group1->nid, ['entity' => $user2, 'state' => OG_STATE_PENDING]);
 
     $this->drupalLogin($user2);
-    $this->drupalPost("user/$user2->uid/edit", array(), 'Save');
+    $this->drupalPost("user/$user2->uid/edit", [], 'Save');
 
-    $this->assertTrue(og_get_entity_groups('user', $user2, array(OG_STATE_PENDING)), 'User membership was retained after user save.');
+    $this->assertTrue(og_get_entity_groups('user', $user2, [OG_STATE_PENDING]), 'User membership was retained after user save.');
   }
 
   /**
@@ -151,23 +151,23 @@ class OgComplexWidgetTest extends WebTestBase {
     $user1 = $this->drupalCreateUser();
 
     // Create a group.
-    $settings = array(
+    $settings = [
       'type' => 'group',
       OG_GROUP_FIELD . '[und][0][value]' => 1,
       'uid' => $user1->uid,
-    );
+    ];
     $group1 = $this->drupalCreateNode($settings);
     $group2 = $this->drupalCreateNode($settings);
 
     // Create group content.
-    $settings = array(
+    $settings = [
       'type' => 'post',
       'uid' => $user1->uid,
-    );
+    ];
     $post1 = $this->drupalCreateNode($settings);
 
-    og_group('node', $group1->nid, array('entity_type' => 'node', 'entity' => $post1, 'field_name' => OG_AUDIENCE_FIELD));
-    og_group('node', $group2->nid, array('entity_type' => 'node', 'entity' => $post1, 'field_name' => 'another_field'));
+    og_group('node', $group1->nid, ['entity_type' => 'node', 'entity' => $post1, 'field_name' => OG_AUDIENCE_FIELD]);
+    og_group('node', $group2->nid, ['entity_type' => 'node', 'entity' => $post1, 'field_name' => 'another_field']);
 
     $this->drupalLogin($user1);
     $this->drupalGet("node/$post1->nid/edit");
