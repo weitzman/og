c15ed4a2b631df183832b957f3d7925408f1e91c
Roy Segall <roy.segall@gizra.com>
2016-03-19 01:18:53 +0200

    Working on the other groups widgets.

--- src/Plugin/Field/FieldWidget/OgComplex.php
+++ src/Plugin/Field/FieldWidget/OgComplex.php
@@ -9,12 +9,14 @@ namespace Drupal\og\Plugin\Field\FieldWidget;
 
 use Drupal\Component\Utility\Html;
 use Drupal\Core\Entity\EntityInterface;
-use Drupal\Core\Field\FieldDefinitionInterface;
 use Drupal\Core\Field\FieldFilteredMarkup;
 use Drupal\Core\Field\FieldItemListInterface;
 use Drupal\Core\Field\FieldStorageDefinitionInterface;
 use Drupal\Core\Field\Plugin\Field\FieldWidget\EntityReferenceAutocompleteTagsWidget;
 use Drupal\Core\Field\Plugin\Field\FieldWidget\EntityReferenceAutocompleteWidget;
+use Drupal\Core\Field\Plugin\Field\FieldWidget\OptionsButtonsWidget;
+use Drupal\Core\Field\Plugin\Field\FieldWidget\OptionsSelectWidget;
+use Drupal\Core\Field\WidgetBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\og\Og;
 use Drupal\og\OgAccess;
@@ -58,7 +60,7 @@ class OgComplex extends EntityReferenceAutocompleteWidget {
 
     // Adding the other groups widget.
     if ($this->isGroupAdmin()) {
-      $parent_form['other_groups'] = $this->otherGroupsWidget($items, $form_state);
+      $parent_form['other_groups'] = $this->otherGroupsWidget($items, $form, $form_state);
     }
 
     return $parent_form;
@@ -121,115 +123,96 @@ class OgComplex extends EntityReferenceAutocompleteWidget {
    * @param $elements
    *   The widget array.
    */
-  protected function otherGroupsWidget(FieldItemListInterface $items, FormStateInterface $form_state) {
-    if ($this->fieldDefinition->getTargetEntityTypeId() == 'user') {
-      $description = $this->t('As groups administrator, associate this user with groups you do <em>not</em> belong to.');
+  protected function otherGroupsWidget(FieldItemListInterface $items, array $form, FormStateInterface $form_state) {
+
+    $widget_id = OgGroupAudienceHelper::getWidgets(
+      $this->fieldDefinition->getTargetEntityTypeId(),
+      $this->fieldDefinition->getTargetBundle(),
+      $this->fieldDefinition->getName(),
+      'admin'
+    );
+
+    $cardinality = $this->fieldDefinition->getFieldStorageDefinition()->getCardinality() !== 1;
+    $element = [
+      '#required' => $this->fieldDefinition->isRequired(),
+      '#multiple' => $cardinality,
+      '#title' => $this->fieldDefinition->getLabel(),
+      '#description' => FieldFilteredMarkup::create(\Drupal::token()->replace($this->fieldDefinition->getDescription())),
+    ];
+
+    $handler = OgGroupAudienceHelper::renderWidget($this->fieldDefinition, $widget_id);
+
+    if ($handler instanceof EntityReferenceAutocompleteWidget && $cardinality) {
+      $widget = $handler->formMultipleElements($items, $form, $form_state);
     }
     else {
-      $description = $this->t('As groups administrator, associate this content with groups you do <em>not</em> belong to.');
+      $widget = $handler->formElement($items, 0, $element, $form, $form_state);
     }
 
-    $field_wrapper = Html::getClass($this->fieldDefinition->getName()) . '-add-another-group';
-
-    $elements = [
-      '#type' => 'container',
-      '#tree' => TRUE,
-      '#title' => $this->t('Other groups'),
-      '#description' => $description,
-      '#prefix' => '<div id="' . $field_wrapper . '">',
-      '#suffix' => '</div>',
-      '#cardinality' => FieldStorageDefinitionInterface::CARDINALITY_UNLIMITED,
-      '#cardinality_multiple' => TRUE,
-      '#theme' => 'field_multiple_value_form',
-      '#field_name' => $this->fieldDefinition->getName(),
-      '#max_delta' => 1,
-    ];
-
-    $elements['add_more'] = [
-      '#type' => 'button',
-      '#value' => $this->t('Add another item'),
-      '#name' => 'add_another_group',
-      '#ajax' => [
-        'callback' => [$this, 'addMoreAjax'],
-        'wrapper' => $field_wrapper,
-        'effect' => 'fade',
-      ],
-    ];
-
-    $delta = 0;
-
-    $target_type = $this->fieldDefinition->getTargetEntityTypeId();
-
-    $user_groups = Og::getEntityGroups(User::load(\Drupal::currentUser()->id()));
-    $user_groups_target_type = isset($user_groups[$target_type]) ? $user_groups[$target_type] : [];
-    $user_group_ids = array_map(function($group) {
-      return $group->id();
-    }, $user_groups_target_type);
-
-    $other_groups_weight_delta = round(count($user_groups) / 2);
-
-    foreach ($items->referencedEntities() as $group) {
-      if (in_array($group->id(), $user_group_ids)) {
-        continue;
-      }
-
-      $elements[$delta] = $this->otherGroupsSingle($delta, $group, $other_groups_weight_delta);
-      $delta++;
-    }
-
-    if (!$form_state->get('other_group_delta')) {
-      $form_state->set('other_group_delta', $delta);
-    }
-
-    // Get the trigger element and check if this the add another item button.
-    $trigger_element = $form_state->getTriggeringElement();
-
-    if ($trigger_element['#name'] == 'add_another_group') {
-      // Increase the number of other groups.
-      $delta = $form_state->get('other_group_delta') + 1;
-      $form_state->set('other_group_delta', $delta);
-    }
-
-    // Add another auto complete field.
-    for ($i = $delta; $i <= $form_state->get('other_group_delta'); $i++) {
-      // Also add one to the weight delta, just to make sure.
-      $elements[$i] = $this->otherGroupsSingle($i, NULL, $other_groups_weight_delta + 1);
-    }
-
-    return $elements;
+    return $this->clearGroups($widget, $this->getOtherGroups(), $handler);
   }
 
   /**
-   * Generating other groups auto complete element.
+   * When rendering a widget we get the referenced groups. This methods will
+   * remove the groups we don't want from the selections.
    *
-   * @param $delta
-   *   The delta of the new element. Need to be the last delta in order to be
-   *   added in the end of the list.
-   * @param EntityInterface|NULL $entity
-   *   The entity object.
-   * @return array
-   *   A single entity reference input.
+   * @param $widget
+   *   The form API element.
+   * @param array $allowed_gids
+   *   The groups IDs need to keep.
+   * @param WidgetBase $handler
+   *   The handler object.
+   *
+   * @return mixed
+   *   Form API element.
    */
-  public function otherGroupsSingle($delta, EntityInterface $entity = NULL, $weight_delta = 10) {
-    return [
-      'target_id' => [
-        // @todo Allow this to be configurable with a widget setting.
-        '#type' => 'entity_autocomplete',
-        '#target_type' => $this->fieldDefinition->getTargetEntityTypeId(),
-        '#selection_handler' => 'og:default',
-        '#selection_settings' => [
+  protected function clearGroups($widget, array $allowed_gids, WidgetBase $handler) {
+
+    if ($handler instanceof OptionsButtonsWidget) {
+      $widget['#options'] = array_filter($widget['#options'], function($key) use($allowed_gids) {
+        return in_array($key, $allowed_gids);
+      }, ARRAY_FILTER_USE_KEY);
+    }
+    elseif ($handler instanceof OptionsSelectWidget) {
+      $widget['#options'] = ['_none' => $this->t('- None -')] + array_filter($widget['#options'], function($key) use($allowed_gids) {
+          return in_array($key, $allowed_gids);
+        }, ARRAY_FILTER_USE_KEY);
+    }
+    elseif ($handler instanceof EntityReferenceAutocompleteWidget) {
+      foreach ($widget as $key => &$value) {
+        if (!is_int($key)) {
+          continue;
+        }
+
+        $value['target_id']['#selection_handler'] = 'og:default';
+        $value['target_id']['#selection_settings'] = [
           'other_groups' => TRUE,
           'field_mode' => 'admin',
-        ],
-        '#default_value' => $entity,
-      ],
-      '_weight' => [
-        '#type' => 'weight',
-        '#title_display' => 'invisible',
-        '#delta' => $weight_delta,
-        '#default_value' => $delta,
-      ],
-    ];
+        ];
+      }
+    }
+
+    return $widget;
+  }
+
+  /**
+   * Get the other groups IDs.
+   *
+   * @return array
+   *   Other groups IDs.
+   *
+   * @throws \Exception
+   */
+  protected function getOtherGroups() {
+    $groups = Og::getSelectionHandler($this->fieldDefinition, ['handler_settings' => ['field_mode' => 'admin']])->getReferenceableEntities();
+
+    $gids = [];
+
+    foreach ($this->getFieldSetting('handler_settings')['target_bundles'] as $target_bundle) {
+      $gids += array_keys($groups[$target_bundle]);
+    }
+
+    return $gids;
   }
 
   /**
